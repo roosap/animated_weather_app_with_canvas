755121def34061c43b673a1d26742eb81c1eec45
roosap <roosa.paivansalo@gmail.com>
2020-02-29 00:54:08 +0000

    Fetch city data from APIs with helper functions

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,43 @@ export default {
       selectedCity: null // change to empty object?
     }
   },
-  components: {
+  mounted() {
+    // fetch('https://api.openweathermap.org/data/2.5/weather?id=6255152&appid=12b0c4fd863f4ff711615b989ae19f37')
+    // .then(res => res.json())
+    // .then(data => this.cities = [...this.cities, this.formatData(data)])
+    this.fetchWeatherData();
+  },
+  methods: {
+    fetchWeatherData() {
+      const urls = [
+        this.cityURL(6255152), // Antarctica
+        this.cityURL(3333229), // Edinburgh
+        this.cityURL(658225), // Helsinki
+        this.cityURL(2636790), // Stornoway
+        this.cityURL(7521757), // Svalbard
+        this.cityURL(1850147), // Tokyo
+        this.cityURL(1791247), // Wuhan
+        this.cityURL(361058), // Alexandria
+        this.cityURL(1070940), // Antananarivo
+        this.cityURL(1819729), // Hong Kong
+        this.cityURL(3067696), // Prague
+        this.cityURL(526346), // Mirnyy
+        this.cityURL(6356055), // Barcelona
+        this.cityURL(3054643), // Budapest
+        this.cityURL(1856215), // Nagano
+        this.cityURL(2648579) // Glasgow
+      ];
+
+      Promise.all(urls.map(url => fetch(url)
+        .then(response => response.json())
+        .then(data => this.cities = [...this.cities, data])
+      ))
+    },
+    cityURL(id){
+      return `https://api.openweathermap.org/data/2.5/weather?id=${id}&appid=12b0c4fd863f4ff711615b989ae19f37`
+    }
   }
+
 }
 </script>
 
